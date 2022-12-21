4d8fb847ff07a8aaf04c02a1e6301a8153563198
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-12-21 16:28:49 -0300

    feat: database increment and new route

--- src/controllers/activitie-controller.ts
+++ src/controllers/activitie-controller.ts
@@ -11,3 +11,12 @@ export async function listDays(req: AuthenticatedRequest, res: Response) {
     return res.sendStatus(httpStatus.NOT_FOUND);
   }
 }
+
+export async function activitiesDay(req: AuthenticatedRequest, res: Response) {
+  try {
+    //const days = await activitiesService.listactivitiesDay(dayId);
+    return res.status(httpStatus.OK).send();
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { listDays } from "@/controllers";
+import { listDays, activitiesDay } from "@/controllers";
 
 const activitiesRoute = Router();
 
 activitiesRoute
   .all("/*", authenticateToken)
-  .get("/day", listDays);
+  .get("/day", listDays)
+  .get("/activities/:dayId", activitiesDay);
+
 export { activitiesRoute };
